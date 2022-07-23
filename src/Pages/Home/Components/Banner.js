3f98ebc36282756c93a9cec9de4452e3b04dc603
anishazahan <anishazahan13@gmail.com>
2022-07-23 18:46:43 +0600

    banner section

---
+++ src/Pages/Home/Components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import bannerImg from "../../../img/online-shopping-concept-shopping-cart-laptop-carton-boxes-e-commerce-big-sales-3d-rendering_634053-7 (1).jpg"
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className='banner' >
+        <div className="banner-content h-100  text-center">
+            <h2 className='banner-heading'>Best Laptop of Your Best Choice!!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, iste velit incidunt sequi iusto molestiae? Architecto praesentium sint inventore placeat.</p>
+           
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner
